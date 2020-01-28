934c4fc582d2187eeabce1f5ef7fc2094ad09fe0
USER <you@example.com>
2020-01-28 19:14:37 +0900

    add new file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, abort
 import os
-import pya3rt
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -10,11 +9,51 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
+import requests
+from bs4 import BeautifulSoup as bs
+import re
+import pickle
+import random
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('QgQu1p3zPbyLJYeg0G4yZzKU8AGaCQbkanX+MutsQPccZDNClUTqgGD7DUh8MGMI/d4N+TnZHVzPf+Jo0Ro94ErSGx/X9JwreK3pflOourtohnsZHj2I1GqZ0TXq7Lz7aPXQrPzmGgfRkRvh/XbMfwdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('979d7d85f9afe2034623bbfa9f9fe47b')
+f = open("./menu_list.txt","rb")
+list_row = pickle.load(f)
+
+#基礎代謝を計算する関数
+def base_energy(tall, weight, age, sex):
+    if sex == 1:
+        result = 13.397 * weight + 4.799 * tall - 5.677 * age + 88.362
+        return result
+    else:
+        result = 9.247 * weight + 3.098 * tall - 4.33 * age + 447.593
+    
+    result = (result * 1.75) -1000
+    return round(result)
+
+#朝と昼・夜のカロリーを計算
+def create_before(aim_kcal):
+    return int(aim_kcal * 0.2)
+def create_after(aim_kcal):    
+    return int(aim_kcal * 0.4)
+
+#メニューをランダムに選ぶ関数
+def make_menu(aim_num):
+    menu = []
+    x = 0
+    for i in range(100):
+        i = random.choice(list_row)
+        if aim_num -100 <= x <= aim_num + 50:
+            return menu
+            break
+        if i[0] + x <= aim_num + 50:
+            x += i[0]
+            menu.append(i)
+        else:
+            continue      
+
 
 
 @app.route("/callback", methods=['POST'])
@@ -38,18 +77,70 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-  reply = create_reply(event.message.text)
-  line_bot_api.reply_message(
-  event.reply_token,
-  TextSendMessage(text=reply))
+    word = event.message.text
+    if word in ["call"]:
+        #朝と昼・夜の基礎代謝データを呼び出し
+        path_r = 'personal_data.txt'
+        with open(path_r) as tf:
+            s = tf.read()
+        morning = int(s[:3])
+        dinner = int(s[3:])
+        today_morning = make_menu(morning)
+        today_lunch = make_menu(dinner)
+        today_dinner= make_menu(dinner)
+        today_menu = []
+        for i in today_morning:
+            today_menu .append( "\n朝ご飯は" + str(i))
+        for i in today_lunch:
+            today_menu.append("\n昼ご飯は" + str(i))
+        for i in today_dinner:
+            today_menu.append("\n夜ご飯は" + str(i))
+        reply = ','.join(today_menu)
 
-def create_reply(user_text):
-  apikey = "DZZ3wYssLsf2MnETFnuxfAjQFgQ1AkAh"
-  client = pya3rt.TalkClient(apikey)
-  res = client.talk(user_text)
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply))
 
-  return res['results'][0]['reply']
+    elif word in ["status"]:
+        setup_text = """以下のフォーマットで入力してください\
+        
+        set:身長-体重-年齢-性別（１男性　or　2女性）
+        ＊全て半角数字をご使用してください
+        ＊例　　　　set:168-68-24-1
+        """
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=setup_text))
 
+    elif 0 <= word.find('set:') :
+        #受けとったデータを身長、体重、年齢、性別に分ける
+        #それをbase_energyに渡す
+        personal_data = event.message.text
+        personal_data = personal_data.replace("set:", "")
+        personal_data = personal_data.replace("-", ",")
+        personal_data = personal_data.split(',')
+        tall = int(personal_data[0])
+        weight = int(personal_data[1])
+        age = int(personal_data[2])
+        sex = int(personal_data[3])
+        aim_kcal = base_energy(tall, weight, age, sex)
+        #各食ごとのカロリーを算出
+        before_noon = create_before(aim_kcal)
+        after_noon = create_after(aim_kcal)
+        #ファイルに書き込み
+        path_w = 'personal_data.txt'
+        with open(path_w, mode='w') as f:
+            f.write(str(before_noon))
+            f.write(str(after_noon))
+
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="設定変更は完了したよ"))
+
+    else:
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="callかstatusと入力してください"))
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
